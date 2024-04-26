e76df0e35b6d028dae15ffb345af777ff9a57a09
MoriFGC <abdulmoha@outlook.it>
2024-04-26 14:45:41 +0200

    aggiunto Footer

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const keyApi = '2RkicnUHCaJ6Jlhyd0tKt6fu6CLQj8nI8lhXYT0k3kIE0zuY1RuRpawg';
 
         chiamata.then((risposta) => risposta.json()).then((rispostaElaborata) => {
            document.getElementById('album').innerHTML = rispostaElaborata.photos.map((image) =>
-        `<div class="col-12 col-md-6 col-lg-4 col-xl-2 ">
+        `<div class="container col-12 col-md-6 col-lg-4 col-xl-4  blur">
                       <div class="card shadow-sm">
                         <img src="${image.src.large}">
                         <div class="card-body">
@@ -16,10 +16,13 @@ const keyApi = '2RkicnUHCaJ6Jlhyd0tKt6fu6CLQj8nI8lhXYT0k3kIE0zuY1RuRpawg';
                           <p class="card-text text-light">
                             Author: ${image.photographer}
                           </p>
-                          <div class="d-flex justify-content-center align-items-center mt-3">
-                            <div class="btn-group">
-                              <button type="button" class="btn btn-sm btn-outline-light"><i class="bi bi-bookmark"></i></button>
-                              <button type="button" class="btn btn-sm btn-outline-light">Download</button>
+                          <div class="d-flex justify-content-between align-items-center mt-3">
+                          <div class="btn-group position-relative">
+                              <button type="button" class="btn btn-sm btn-outline-light"><i class="bi bi-hand-thumbs-up"></i><i class="bi bi-hand-thumbs-up-fill"></i></button>
+                            </div>
+                            <div class="btn-group position-relative ">
+                              <button type="button" class="btn btn-sm btn-outline-light bottoni-card"><i class="bi bi-bookmark"></i><i class="bi bi-bookmark-fill"></i></button>
+                              <button type="button" class="btn btn-sm btn-outline-light bottoni-card">Download</button>
                             </div>
                           </div>
                         </div>
